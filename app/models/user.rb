ffc8f8a704015a136b996d8b48cb93ba2d555eff
reityeru <reityeru0084055@gmail.com>
2021-02-19 17:09:21 +0900

    ユーザー認証機能、ページ遷移、ログアウト機能の実装、バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birthday, presence: true
+  # validates :email, presence: true, uniqueness: { case_sensitive: true }
+
+
+
+
+
+
 end
