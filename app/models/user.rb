0db9a59216e42bb8a6891071a7d1e41623da2093
reityeru <reityeru0084055@gmail.com>
2021-02-22 00:32:51 +0900

    不要なコメントアウトの削除、バリデーションの整理

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,21 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :nickname, presence: true
-  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/}
-  validates :first_name, presence: true,format: {with: /\A[ぁ-んァ-ン一-龥]+\z/}
-  validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
-  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
-  validates :birthday, presence: true
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
-  validates :email, format: { with: /\A\S+@\S+\.\S+\z/}
   
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    validates :email, format: { with: /\A\S+@\S+\.\S+\z/}
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+  
+    with_options format: {with: /\A[ぁ-んァ-ン一-龥々]+\z/} do
+      validates :last_name
+      validates :first_name
+    end
 
-
-
-
-
-
+    with_options format: {with: /\A[ァ-ヶー－]+\z/} do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+  end
 end
