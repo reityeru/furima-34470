bb187205e6eb6cb0d9048eb6b776f4563e4c2b14
reityeru <reityeru0084055@gmail.com>
2021-02-22 19:10:44 +0900

    モデル作成、アソシエーション、バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,33 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :item_name, length: { maximum: 40 }
+    validates :iteme_text, length: { maximum: 1000 }
+    validates :item_price
+    validates :item_category_id
+    validates :item_status_id
+    validates :shipping_charges_id
+    validates :shipping_area_id
+    validates :day_to_ship_id
+
+    with_options numericality: { other_than: 1 }  do
+      validates :item_category_id
+      validates :item_status_id
+      validates :shipping_charges_id
+      validates :shipping_area_id
+      validates :day_to_ship_id
+    end
+
+  end
+
   has_one_attached :image
+
+  belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :item_category
+  belongs_to :item_status
+  belongs_to :shipping_charges
+  belongs_to :shipping_area
+  belongs_to :day_to_ship
+
 end
