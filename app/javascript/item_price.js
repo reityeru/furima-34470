ecfef1e95d4aaa26ee932daf77505a1e9a5695df
reityeru <reityeru0084055@gmail.com>
2021-02-23 12:39:37 +0900

    不要なconsole.logの削除

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -3,17 +3,14 @@ function item_price() {
   
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    console.log(inputValue);
 
     const salesCommission = document.getElementById("add-tax-price")
     salesCommission.innerHTML = (Math.floor(inputValue * 0.1));
-    console.log(salesCommission);
     
     const salesProfit = document.getElementById("profit")
     const calculationResult = inputValue * 0.1
     console.log(calculationResult);
     salesProfit.innerHTML =(Math.floor(inputValue - calculationResult));
-    console.log(salesProfit);
  });
 }
 
