68bba7ff39b68b5e420ee5df09b4a2be1e1a35fd
reityeru <reityeru0084055@gmail.com>
2021-02-23 14:09:53 +0900

    integer型の為、""の削除

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -47,7 +47,7 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include("Item category can't be blank", "Item category can't be blank", "Item category is not a number")
     end
     it "カテゴリーの選択が「---」では出品できないこと" do
-      @item.item_category_id = "1"
+      @item.item_category_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Item category must be other than 1")
     end
@@ -58,7 +58,7 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include("Item status can't be blank", "Item status can't be blank", "Item status is not a number")
     end
     it "商品の状態の選択が「---」では出品できないこと" do
-      @item.item_status_id = "1"
+      @item.item_status_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Item status must be other than 1")
     end
@@ -69,7 +69,7 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include("Shipping charges can't be blank", "Shipping charges can't be blank", "Shipping charges is not a number")
     end
     it "配送料の負担の選択が「---」では出品できないこと" do
-      @item.shipping_charges_id = "1"
+      @item.shipping_charges_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Shipping charges must be other than 1")
     end
@@ -80,7 +80,7 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include("Shipping area can't be blank", "Shipping area can't be blank", "Shipping area is not a number")
     end
     it "配送元の地域の選択が「---」では出品できないこと" do
-      @item.shipping_area_id = "1"
+      @item.shipping_area_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Shipping area must be other than 1")
     end
@@ -91,7 +91,7 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include("Day to ship can't be blank", "Day to ship can't be blank", "Day to ship is not a number")
     end
     it "発送までの日時の選択が「---」では出品できないこと" do
-      @item.day_to_ship_id = "1"
+      @item.day_to_ship_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Day to ship must be other than 1")
     end
@@ -103,25 +103,25 @@ RSpec.describe Item, type: :model do
     end
 
     it "価格が整数でなければ出品できないこと" do
-      @item.item_price = "300.5"
+      @item.item_price = 300.5
       @item.valid?
       expect(@item.errors.full_messages).to include("Item price must be an integer")
     end
 
     it "価格が300円未満では出品できないこと" do
-      @item.item_price = "299"
+      @item.item_price = 299
       @item.valid?
       expect(@item.errors.full_messages).to include("Item price must be greater than or equal to 300")
     end
 
     it "価格が9999999円より大きい場合は出品できないこと" do
-      @item.item_price = "10000000"
+      @item.item_price = 10000000
       @item.valid?
       expect(@item.errors.full_messages).to include("Item price must be less than or equal to 9999999")
     end
 
     it "価格が半角数字のみでしか出品できないこと" do
-      @item.item_price = "３００"
+      @item.item_price = ３００
       @item.valid?
       expect(@item.errors.full_messages).to include("Item price is not a number")
     end
