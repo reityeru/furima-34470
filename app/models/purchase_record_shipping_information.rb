f430471b2edb304fedf3b6967a5d04a1978a8f0c
reityeru <reityeru0084055@gmail.com>
2021-02-25 12:30:10 +0900

    フォームオブジェクト作成

---
+++ app/models/purchase_record_shipping_information.rb
@@ -0,0 +1,23 @@
+class PurchaseRecordShippingInformation
+  include ActiveModel::Model
+  attr_accessor :postal_code, :shipping_area_id, :municipality, :address, :building_name, :phone_number, :purchase_record_id, :user_id, :item_id
+
+# ここにバリデーションの処理を書く
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+    validates :shipping_area_id, numericality: { other_than: 1 }
+    validates :municipality
+    validates :address
+    validates :phone_number, format: {with: /\A\d{10,11}\z/}
+    validates :purchase_record_id
+    validates :user_id
+    validates :itam_id
+  end
+
+  def save
+    # 各テーブルにデータを保存する処理を書く
+    purchase_record = PurchaseRecord.create(item_id: item_category_id, user_id: user_id)
+
+    ShippingInformation.create(postal_code: postal_code, shipping_area_id: shipping_area_id, municipality: municipality, address: address, building_name: building_name, phone_number, purchase_record_id: purchase_record.id)
+  end
+end
