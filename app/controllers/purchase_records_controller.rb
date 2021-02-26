44f428488287b31b24b020efbb1059bd6534408c
reityeru <reityeru0084055@gmail.com>
2021-02-26 19:06:24 +0900

    決済機能実装

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -5,12 +5,13 @@ class PurchaseRecordsController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @purchase_record_shipping_information = PurchaseRecordShippingInformation.new(purchase_record_shipping_information_params)
     if @purchase_record_shipping_information.valid?
+      pay_item
       @purchase_record_shipping_information.save
       redirect_to root_path
     else
-      @item = Item.find(params[:item_id])
       render :index
     end
   end
@@ -19,6 +20,15 @@ class PurchaseRecordsController < ApplicationController
   private
 
   def purchase_record_shipping_information_params
-    params.require(:purchase_record_shipping_information).permit(:postal_code, :shipping_area_id, :municipality, :address, :building_name, :phone_number, :number, :exp_month, :exp_year, :cvc).merge(user_id: current_user.id,  item_id: params[:item_id])
+    params.require(:purchase_record_shipping_information).permit(:postal_code, :shipping_area_id, :municipality, :address, :building_name, :phone_number, :number, :exp_month, :exp_year, :cvc).merge(user_id: current_user.id,  item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.item_price,
+        card: purchase_record_shipping_information_params[:token],
+        currency: 'jpy'
+      )
   end
 end
